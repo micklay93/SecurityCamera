637b6e8eb415b66555f233b3333c6fb0b640b32d
micklay93 <39902237+micklay93@users.noreply.github.com>
2021-03-06 17:57:12 +0000

    Update footageRetentionPolicy.sh

--- Scripts/footageRetentionPolicy.sh
+++ Scripts/footageRetentionPolicy.sh
@@ -1,12 +1,11 @@
+#!/bin/sh
+
+echo "STARTING footageDeletionPolicy.sh" >> ~/footageDeletionLog.txt;
+
 while true; do
-    oldDate=$(busybox date -D %s -d $(( $(busybox date +%s) - 604800)) '+%YY%mM%dD')
-    #oldDate is the date 7 days ago
-    echo $oldDate  >> footageDeletionLog.txt ;
-    echo "BEFORE DELETE:" >> footageDeletionLog.txt ;
-    ls /tmp/sd/record/ >> footageDeletionLog.txt ;
-    echo "rm -rf /tmp/sd/record/$oldDate*" >> footageDeletionLog.txt ;
-    rm -rf /tmp/sd/record/$oldDate* >> footageDeletionLog.txt ; #Comment this line out to stop it deleting files
-    echo "AFTER DELETE:" >> footageDeletionLog.txt ;
-    ls /tmp/sd/record/ >> footageDeletionLog.txt ;
     sleep 86400;
+    echo "DELETING FILES OLDER THAN 3 DAYS" >> ~/footageDeletionLog.txt;
+    find /tmp/sd/record/* -mtime +3;
+    find /tmp/sd/record/* -mtime +3 -exec rm {} \;
+    echo "COMPLETED" >> ~/footageDeletionLog.txt;
 done &
